6c68e2aaba3fc339234face81793ded60af3d267
oereo <dlstpgns0406@gmail.com>
2020-01-10 03:59:56 +0900

    add postboard in home.html

--- blog/models.py
+++ blog/models.py
@@ -3,14 +3,44 @@ from datetime import datetime
 
 #Create your models here.
 
+# class Blog(models.Model):
+#     title = models.CharField(max_length = 200)
+#     pub_date = models.DateTimeField('date published')
+#     body = models.TextField()
+
+#     def __str__(self):
+#         return self.title
+
+#     def summary(self):
+#         return self.body[:100]
+
+# class Comment(models.Model):
+#     blog = models.ForeignKey('blog.Blog', on_delete=models.CASCADE, related_name='comments')
+#     author = models.CharField(max_length=200)
+#     text = models.TextField()
+
+#     def __str__(self):
+#         return self.text
+
 class Blog(models.Model):
-    title = models.CharField(max_length = 200)
-    pub_date = models.DateTimeField('date published')
+    Id = models.IntegerField()
+    # owner = models.CharField(max_length=20, default="???")
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date_published')
     body = models.TextField()
+    # views = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
 
-    def summary(self):
-        return self.body[:100]
+    def increaseViews(self):
+        self.views +=1
+        self.save()
 
+class Comment(models.Model):
+    blog = models.ForeignKey('blog.Blog', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 from django.utils import timezone
 from .models import Blog
+from .models import Comment
 from django.contrib.auth.models import User
 from django.contrib import auth
 
@@ -10,7 +11,7 @@ from django.contrib import auth
 def home(request):
     blogs = Blog.objects #쿼리셋 - 전달받은 객체 #메소드
     blog_list = Blog.objects.all()
-    paginator = Paginator(blog_list, 3)
+    paginator = Paginator(blog_list, 8)
     page = request.GET.get('page')
     posts = paginator.get_page(page)
     return render(request, 'home.html', {'blogs': blogs, 'posts' : posts})
@@ -33,9 +34,10 @@ def create(request):
     blog.pub_date = timezone.datetime.now()
     blog.save()
     return redirect('/blog/'+str(blog.id))
-    # return redirect('null')
-    # return render(request, 'home.html')
+    return redirect('null')
+    return render(request, 'home.html')
     # 입력받은 내용을 데이터 베이스에 넣어주는 함수
+
 
 def mypage(request):
     user = request.user
