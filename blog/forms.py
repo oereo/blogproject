dcc112135127befd512bd5fece4b4a2843a9aeaf
oereo <dlstpgns0406@gmail.com>
2020-01-17 16:05:01 +0900

    final blog

--- blog/forms.py
+++ blog/forms.py
@@ -6,7 +6,7 @@ from .models import Photo
 #     image = forms.ImageField()
 
 class UploadForm(forms.ModelForm):
-    comment = forms.CharField(max_length=255)
+    # comment = forms.CharField(max_length=255)
     class Meta:
         model = Photo                         # 어떤 모델과 연결할지
         exclude = ('thumnail_image', 'owner') # 입력받지 않을 필드를 표시가능, 이 부분은 코드로 처리해주기 위해
